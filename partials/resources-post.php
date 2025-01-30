2dfcdedb57f518c88f3c2b0aa2a49785edd96bf9
Izzudin Fasya <izzudinfasya@gmail.com>
2025-01-31 03:59:03 +0700

    video page

--- partials/resources-post.php
+++ partials/resources-post.php
@@ -1,6 +1,10 @@
 <?php
 $resources = get_field('post_details');
-$author_avatar = $resources['company_logo']['url'] ? $resources['company_logo']['url'] : get_template_directory_uri() . '/assets/icons/si_logo.jpg';
+// $author_avatar = $resources['company_logo']['url'] ? $resources['company_logo']['url'] : get_template_directory_uri() . '/assets/icons/si_logo.jpg';
+$author_avatar = (is_array($resources) && isset($resources['company_logo']['url']) && $resources['company_logo']['url'])
+    ? $resources['company_logo']['url']
+    : get_template_directory_uri() . '/assets/icons/si_logo.jpg';
+
 $categories = get_the_category();
 $cat_icon = get_field('category_icon', 'term_' . $categories[0]->term_id);
 ?>
@@ -28,7 +32,10 @@ $cat_icon = get_field('category_icon', 'term_' . $categories[0]->term_id);
             <?php echo '<p class="excerpt">' . get_the_excerpt() . '</p>' ?>
             <?php if ($resources): ?>
                 <div class="entry-details !mt-3">
-                    <img src="<?php echo $author_avatar; ?>" alt="<?php echo $resources['company_logo']['alt']; ?>" />
+                    <!-- <img src="<?php echo $author_avatar; ?>" alt="<?php echo $resources['company_logo']['alt']; ?>" /> -->
+                    <img src="<?php echo esc_url($author_avatar); ?>"
+                        alt="<?php echo (is_array($resources) && isset($resources['company_logo']['alt'])) ? esc_attr($resources['company_logo']['alt']) : 'Default Alt Text'; ?>" />
+
                     <div class="author-title">
                         <p class="font-medium"><?= $resources['author'] ? $resources['author'] : 'Strategy Institute'; ?></p>
                         <p class="text-neutral-500"><?php echo $resources['job_title']; ?></p>
